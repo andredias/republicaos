7c41f10ce88737db42bdabddb4de7f7a57059bd4
andref <none@none>
2007-03-07 01:00:00 +0000

    Aprimoramento do modelo usando SQLAlchemy.
    A idéia era aprender a definir o modelo usando o SQLAlchemy e só depois passar para o Elixir. Entretanto, o SQLAlchemy está mais complicado do que vale a pena.

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from sqlalchemy import *
 
 db       = create_engine( 'sqlite:///republique.db' )
 metadata = BoundMetaData( db )
+session  = create_session( bind_to = db )
 
 pessoa_table = Table( 'pessoa', metadata,
 	Column( 'id', Integer, primary_key = True ),
@@ -9,32 +10,40 @@ pessoa_table = Table( 'pessoa', metadata,
 )
 
 contato_table = Table('contato', metadata,
-	Column( 'id_pessoa', Integer, ForeignKey( 'pessoa.id' ), primary_key = True ),
 	Column( 'id_contato', Integer, primary_key = True ),
 	Column( 'contato', String( 100 ), nullable = False ),
-	Column( 'tipo_contato', Integer, nullable = False )
+	Column( 'tipo_contato', Integer, nullable = False ),
+	Column( 'id_pessoa', Integer, ForeignKey( 'pessoa.id' ), nullable = False )
 )
 
 responsavel_telefonema_table = Table( 'responsavel_telefonema', metadata,
-	Column( 'id_pessoa', Integer, primary_key = True ),
+	Column( 'id_pessoa', Integer, ForeignKey( 'pessoa.id' ), primary_key = True ),
 	Column( 'telefone', String( 12 ), primary_key = True ),
-	Column( 'descricao', String ),
-	ForeignKeyConstraint( ['id_pessoa'], ['pessoa.id'] )
+	Column( 'descricao', String )
 )
 
 republica_table = Table( 'republica', metadata,
 	Column( 'id', Integer, primary_key = True ),
 	Column( 'nome', String( 80 ), nullable = False ),
 	Column( 'data_criacao', Date, default = func.now(), nullable = False ),
-	Column( 'ult_fechamento', Date ),
-	Column( 'prox_fechamento', Date )
+	Column( 'logradouro', String( 150 )),
+	Column( 'complemento', String( 100 )),
+	Column( 'bairro', String( 100 )),
+	Column( 'cidade', String( 80 )),
+	Column( 'uf', String( 2 )),
+	Column( 'cep', String( 8 ))
+)
+
+fechamento_table = Table( 'fechamento', metadata,
+    Column( 'id_republica', Integer, ForeignKey( 'republica.id' ), primary_key = True ),
+    Column( 'data', Date, primary_key = True )
 )
 
 conta_telefone_table = Table( 'conta_telefone', metadata, 
-		Column( 'id', Integer, primary_key = True ),
-		Column( 'id_republica', Integer, ForeignKey( 'republica.id' ), nullable = False ),
-		Column( 'companhia', Integer, nullable = False ),
-		Column( 'telefone', String( 12 ), nullable = False )
+	Column( 'id', Integer, primary_key = True ),
+	Column( 'id_republica', Integer, ForeignKey( 'republica.id' ), nullable = False ),
+	Column( 'companhia', Integer, nullable = False ),
+	Column( 'telefone', String( 12 ), nullable = False )
 )
 
 telefonema_table = Table( 'telefonema', metadata,
@@ -55,17 +64,96 @@ morador_table = Table( 'morador', metadata,
 
 tipo_despesa_table = Table( 'tipo_despesa', metadata,
 	Column( 'id', Integer, primary_key = True ),
-	Column( 'id_republica', Integer, ForeignKey( 'republica.id' )),
+	Column( 'id_republica', Integer, ForeignKey( 'republica.id' ), nullable = False),
 	Column( 'tipo', String(40), nullable = False ),
 	Column( 'descricao', String )
 )
 
+# manter id_republica na tabela para facilitar a busca
 despesa_table = Table( 'despesa', metadata,
 	Column( 'id', Integer, primary_key = True ),
 	Column( 'data', Date, default = func.now(), nullable = False ),
 	Column( 'valor', Numeric( 8, 2 ), nullable = False ),
-	Column( 'id_republica', Integer, ForeignKey( 'republica.id' ), nullable = False ),
+	Column( 'id_republica', Integer, nullable = False ),
 	Column( 'id_pessoa', Integer, ForeignKey( 'pessoa.id' ), nullable = False ),
-	Column( 'id_tipo_despesa', Integer, ForeignKey( 'tipo_despesa.id' ), nullable = False )
+	Column( 'id_tipo_despesa', Integer, nullable = False ),
+	ForeignKeyConstraint(
+	    ['id_tipo_despesa', 'id_republica'],
+	    ['tipo_despesa.id', 'tipo_despesa.id_republica']
+	)
 )
+
+
+#
+# ORM
+#
+
+from sqlalchemy.orm import *
+
+
+class Pessoa(object):
+    pass
+
+class Contato(object):
+    pass
+
+class ResponsavelTelefonema(object):
+    pass
+
+class Republica(object):
+    pass
+
+class Fechamento(object):
+    pass
+
+class ContaTelefone(object):
+    pass
+
+class Telefonema(object):
+    pass
+
+class Morador(object):
+    pass
+
+class TipoDespesa(object):
+    pass
+
+class Despesa(object):
+    pass
+
+
+#
+# Mappers
+#
+
+mapper( Pessoa, pessoa_table )
+mapper( Republica, republica_table )
+
+mapper( Contato, contato_table,
+    properties = { 
+		'pessoa':relation( Pessoa, backref = backref( 'contatos', cascade='all, delete-orphan' ))
+	}
+)
+
+mapper( ResponsavelTelefonema, responsavel_telefonema_table, properties = { 
+    'pessoa':relation( Pessoa, backref = backref( 'resp_por_telefones', cascade='all, delete-orphan' ))
+	}
+)
+
+mapper( Fechamento, fechamento_table, properties = {
+    'republica':relation( Republica, backref = backref( 'fechamentos', cascade='all, delete-orphan' ))
+	}
+)
+
+mapper( TipoDespesa, tipo_despesa_table, properties = { 'republica':relation( Republica )})
+mapper( ContaTelefone, conta_telefone_table,
+    properties = { 'republica':relation( Republica, backref=backref( 'telefone' ))}
+)
+
+#mapper( Despesa
+#mapper( Telefonema
+#mapper( Morador
+
+
+
 
