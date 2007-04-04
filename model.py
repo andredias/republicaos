ec62e1ed82e36dc2a4c73046e8b83c6ca138faf7
andref <none@none>
2007-04-04 01:14:51 +0000

    Evolução do projeto:
     1. Definido o modelo de classes usando o Elixir
     1. Alguns métodos já definidos
     1. test_model.py possui alguns testes iniciais para serem rodados com o
    nosetests

--- model.py
+++ model.py
@@ -1,102 +1,141 @@
+# -*- coding: utf-8 -*-
+
 from elixir      import Unicode, Date, DateTime, Time, String, Integer, Numeric
 from elixir      import Entity, has_field, using_options, using_table_options, using_mapper_options
 from elixir      import has_many as one_to_many
 from elixir      import belongs_to as many_to_one
 from elixir      import has_and_belongs_to_many as many_to_many
 from elixir      import metadata
-from sqlalchemy  import ForeignKey, ForeignKeyConstraint
-from datetime    import datetime
+from sqlalchemy  import *
+from datetime    import date, datetime
+import time
 
-metadata.connect( 'sqlite:///' )
-metadata.engine.echo = True
+class Pessoa(Entity):
+	has_field('nome', Unicode(80), nullable = False, unique = True)
+	using_options(tablename = 'pessoa')
+	one_to_many('contatos', of_kind = 'Contato', inverse = 'pessoa')
+	one_to_many('telefones', of_kind = 'Telefone', inverse = 'responsavel')
 
-class Pessoa( Entity ):
-    has_field( 'nome', Unicode( 80 ), nullable = False, unique = True )
-    using_options( tablename = 'pessoa' )
-    one_to_many( 'contatos', of_kind = 'Contato', inverse = 'pessoa' )
-    one_to_many( 'responsavel_por_telefones', of_kind = 'ResponsavelPorTelefone', inverse = 'responsavel' )
+class Contato(Entity):
+	has_field('contato', Unicode(100), nullable = False),
+	has_field('tipo', Integer, nullable = False),
+	many_to_one('pessoa', of_kind = 'Pessoa', inverse = 'contatos', colname = 'id_pessoa',
+		column_kwargs = dict(nullable = False))
+	using_options(tablename = 'contato')
 
-class Contato( Entity ):
-    has_field( 'contato', String( 100 ), nullable = False ),
-    has_field( 'tipo', Integer, nullable = False ),
-    many_to_one( 'pessoa', of_kind = 'Pessoa', inverse = 'contatos' )
-    using_options( tablename = 'contato' )
+class Telefone(Entity):
+	has_field('numero', Numeric(10, 0), primary_key = True)
+	has_field('descricao', Unicode)
+	using_options(tablename = 'telefone')
+	many_to_one('responsavel', of_kind = 'Pessoa',
+		inverse = 'telefones', colname = 'id_pessoa_resp',
+		column_kwargs = dict(primary_key = True))
 
-class ResponsavelPorTelefone( Entity ):
-    has_field( 'telefone', Integer, primary_key = True )
-    has_field( 'descricao', Unicode )
-    using_options( tablename = 'responsavel_telefone' )
-    many_to_one( 'responsavel', of_kind = 'Pessoa',
-	    inverse = 'responsavel_por_telefones', colname = 'id_pessoa_resp',
-	    column_kwargs = dict( primary_key = True ))
-
-class Republica( Entity ):
-    has_field( 'nome', Unicode( 80 ), nullable = False )
-    has_field( 'data_criacao', Date, default = datetime.now, nullable = False )
-    has_field( 'logradouro', Unicode( 150 ))
-    has_field( 'complemento', Unicode( 100 ))
-    has_field( 'bairro', Unicode( 100 ))
-    has_field( 'cidade', Unicode( 80 ))
-    has_field( 'uf', String( 2 ))
-    has_field( 'cep', String( 8 ))
-    using_options( tablename = 'republica' )
-    one_to_many( 'fechamentos', of_kind = 'Fechamento', inverse = 'republica' )
-    one_to_many( 'contas_telefone', of_kind = 'ContaTelefone', inverse = 'republica' )
-    one_to_many( 'tipos_despesa', of_kind = 'TipoDespesa', inverse = 'republica' )
-    
-    def moradores( data_inicial = None, data_final = datetime.now() ):
-	    pass
-
-class Fechamento( Entity ):
-    has_field( 'data', Date, primary_key = True )
-    using_options( tablename = 'fechamento' )
-    many_to_one( 'republica', of_kind = 'Republica', inverse = 'fechamentos', colname = 'id_republica',
-	    column_kwargs = dict( primary_key = True ))
-
-class ContaTelefone( Entity ):
-    has_field( 'companhia', Integer, nullable = False )
-    has_field( 'telefone', Integer, nullable = False )
-    has_field( 'dia_vencimento', Integer, nullable = False )
-    using_options( tablename = 'conta_telefone' )
-    many_to_one( 'republica', of_kind = 'Republica', inverse = 'contas_telefone',
-	    column_kwargs = dict( nullable = False ))
-#    one_to_many( 'telefonemas', of_kind = 'Telefonema', inverse = 'conta_telefone' )
-    def telefonemas( periodo ):
-	    pass
-
-class Telefonema( Entity ):
-    has_field( 'periodo_ref', Integer, primary_key = True )
-    has_field( 'telefone', Integer, primary_key = True )
-    has_field( 'tipo_fone', Integer, nullable = False )			# fixo, celular, net fone
-    has_field( 'tipo_distancia', Integer, nullable = False )	# Local, DDD, DDI
-    has_field( 'duracao', Time, nullable = False )
-    has_field( 'valor', Numeric( 8, 2 ), nullable = False )
-    using_options( tablename = 'telefonema' )
-    many_to_one( 'responsavel', of_kind = 'Pessoa', inverse = 'telefonemas')
-    many_to_one( 'conta_telefone', of_kind = 'ContaTelefone', inverse = 'telefonemas',
-	    colname = 'id_conta_telefone', column_kwargs = dict( primary_key = True ))
-
-class Morador( Entity ):
-    has_field( 'data_entrada', Date, default = datetime.now, primary_key = True )
-    has_field( 'data_saida', Date )
-    using_options( tablename = 'morador' )
-    many_to_one( 'republica', of_kind = 'Republica', inverse = 'moradores', colname = 'id_republica',
-	    column_kwargs = dict( primary_key = True ))
-    many_to_one( 'pessoa', of_kind = 'Pessoa', colname = 'id_pessoa',
-	    column_kwargs = dict( primary_key = True ))
-
-class TipoDespesa( Entity ):
-    has_field( 'tipo', Unicode(40), nullable = False )
-    has_field( 'descricao', String )
-    using_options( tablename = 'tipo_despesa' )
-    many_to_one( 'republica', of_kind = 'Republica', inverse = 'tipo_despesas',
-		column_kwargs = dict( nullable = False ))
+class Republica(Entity):
+	has_field('nome', Unicode(80), nullable = False)
+	has_field('data_criacao', Date, default = date.today, nullable = False)
+	has_field('logradouro', Unicode(150))
+	has_field('complemento', Unicode(100))
+	has_field('bairro', Unicode(100))
+	has_field('cidade', Unicode(80))
+	has_field('uf', String(2))
+	has_field('cep', String(8))
+	using_options(tablename = 'republica')
+	one_to_many('fechamentos', of_kind = 'Fechamento', inverse = 'republica')
+	one_to_many('contas_telefone', of_kind = 'ContaTelefone', inverse = 'republica')
+	one_to_many('tipos_despesa', of_kind = 'TipoDespesa', inverse = 'republica')
 
 
-class Despesa( Entity ):
-    has_field( 'data', Date, default = datetime.now, nullable = False )
-    has_field( 'valor', Numeric( 8, 2 ), nullable = False )
-    using_options( tablename = 'despesa' )
-    many_to_one( 'pessoa', of_kind = 'Pessoa', column_kwargs = dict( nullable = False ))
-    many_to_one( 'republica', of_kind = 'Republica', column_kwargs = dict( nullable = False ))
-    many_to_one( 'tipo_despesa', of_kind = 'TipoDespesa', column_kwargs = dict( nullable = False ))
+	def ultimo_fechamento(self):
+		'''
+		Último fechamento da república. Caso não haja nenhum, retorna a data em que a república foi criada
+		'''
+		
+		# retorna uma string com a data e não um objeto datetime
+		# pelo menos no SQLite
+		data = select(
+				[func.max(Fechamento.c.data)],
+				Fechamento.c.id_republica == self.id
+			).execute().fetchone()[0]
+		
+		if data == None:
+			data = self.data_criacao
+		elif type(data) != type(self.data_criacao):
+			# transforma a string em data
+			partes = time.strptime(data,'%Y-%m-%d')
+			data = date(partes[0], partes[1], partes[2])
+		return data
+
+
+	def moradores(self, data_inicial = None, data_final = None):
+		'''
+		Retorna os moradores da república no período de tempo
+		'''
+		if not data_inicial:
+			data_inicial = self.ultimo_fechamento()
+		# não sei se por data.today() como valor default garante que vai ser calculado a cada chamada
+		if not data_final:
+			data_final = date.today()
+		return Pessoa.select(
+					and_(
+						Morador.c.id_republica == self.id,
+						Pessoa.c.id == Morador.c.id_pessoa,
+						Morador.c.data_entrada < data_final,
+						or_(Morador.c.data_saida >= data_inicial, Morador.c.data_saida == None)
+					)
+				)
+
+
+class Fechamento(Entity):
+	has_field('data', Date, primary_key = True)
+	using_options(tablename = 'fechamento')
+	many_to_one('republica', of_kind = 'Republica', inverse = 'fechamentos', colname = 'id_republica',
+		column_kwargs = dict(primary_key = True))
+
+class ContaTelefone(Entity):
+	has_field('companhia', Integer, nullable = False)
+	has_field('telefone', Numeric(10, 0), nullable = False)
+	has_field('dia_vencimento', Integer, nullable = False)
+	using_options(tablename = 'conta_telefone')
+	many_to_one('republica', of_kind = 'Republica', inverse = 'contas_telefone',
+		column_kwargs = dict(nullable = False))
+#    one_to_many('telefonemas', of_kind = 'Telefonema', inverse = 'conta_telefone')
+	def telefonemas(periodo):
+		pass
+
+class Telefonema(Entity):
+	has_field('periodo_ref', Integer, primary_key = True)
+	has_field('telefone', Numeric(10, 0), primary_key = True)
+	has_field('tipo_fone', Integer, nullable = False)			# fixo, celular, net fone
+	has_field('tipo_distancia', Integer, nullable = False)	# Local, DDD, DDI
+	has_field('duracao', Time, nullable = False)
+	has_field('valor', Numeric(8, 2), nullable = False)
+	using_options(tablename = 'telefonema')
+	many_to_one('responsavel', of_kind = 'Pessoa', inverse = 'telefonemas')
+	many_to_one('conta_telefone', of_kind = 'ContaTelefone', inverse = 'telefonemas',
+		colname = 'id_conta_telefone', column_kwargs = dict(primary_key = True))
+
+class Morador(Entity):
+	has_field('data_entrada', Date, default = date.today, primary_key = True)
+	has_field('data_saida', Date)
+	using_options(tablename = 'morador')
+	many_to_one('republica', of_kind = 'Republica', inverse = 'moradores', colname = 'id_republica',
+		column_kwargs = dict(primary_key = True))
+	many_to_one('pessoa', of_kind = 'Pessoa', colname = 'id_pessoa',
+		column_kwargs = dict(primary_key = True))
+
+class TipoDespesa(Entity):
+	has_field('tipo', Unicode(40), nullable = False)
+	has_field('descricao', String)
+	using_options(tablename = 'tipo_despesa')
+	many_to_one('republica', of_kind = 'Republica', inverse = 'tipo_despesas',
+		column_kwargs = dict(nullable = False))
+
+
+class Despesa(Entity):
+	has_field('data', Date, default = date.today, nullable = False)
+	has_field('valor', Numeric(8, 2), nullable = False)
+	using_options(tablename = 'despesa')
+	many_to_one('pessoa', of_kind = 'Pessoa', column_kwargs = dict(nullable = False))
+	many_to_one('republica', of_kind = 'Republica', column_kwargs = dict(nullable = False))
+	many_to_one('tipo_despesa', of_kind = 'TipoDespesa', column_kwargs = dict(nullable = False))
--- test_model.py
+++ test_model.py
@@ -1,16 +1,98 @@
-import model_elixir
-from sqlalchemy import create_engine
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-class TestaModelo( object ):
-    def setup(self):
-        engine = create_engine('sqlite:///')
-        metadata.connect(engine)
-        create_all()
+from model import *
+from elixir import *
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
-    def tear_down(self):
-	    cleanup_all()
-	    
-    def test_populando(self):
-	    p1 = Pessoa( nome = 'Felipe' )
-	   
-	    
+def setup():
+	metadata.connect('sqlite:///')
+	metadata.engine.echo = True
+	create_all()
+	p1 = Pessoa(nome = 'André')
+	p2 = Pessoa(nome = 'Marcos')
+	
+	t1 = Telefone(numero = 1932730483, descricao = 'Casa da Andréa')
+	t2 = Telefone(numero = 1996338801, descricao = 'Celular da Andréa')
+	t3 = Telefone(numero = 1239134306, descricao = 'Casa dos pais')
+	
+	t1.responsavel = p1
+	t2.responsavel = p1
+	t3.responsavel = p2
+	
+	r = Republica(nome = 'Jerônimo',
+				data_criacao = date(year = 1998, month = 02, day = 01),
+				logradouro = 'R. Jerônimo Pattaro, 186',
+				bairro = 'Barão Geraldo',
+				cidade = 'Campinas',
+				uf = 'SP',
+				cep = '13084110')
+	
+	Morador(
+		pessoa = p1,
+		republica = r,
+		data_entrada = date(1998, 02, 01)
+	)
+
+	Morador(
+		pessoa = p2,
+		republica = r,
+		data_entrada = date(2005, 10, 13)
+	)
+	
+	objectstore.flush()
+
+def teardown(self):
+	cleanup_all()
+
+
+class TestaModelo(object):
+	#def teardown(self):
+		## we don't use cleanup_all because setup and teardown are called for 
+		## each test, and since the class is not redefined, it will not be
+		## reinitialized so we can't kill it
+		#drop_all()
+	
+	def test_responsavel_por_telefonemas(self):
+		p1 = Pessoa.get_by(nome = 'André')
+		
+		t = Telefone.get_by(numero = 1932730483)
+		assert t.responsavel is p1
+		
+		t = Telefone.get_by(numero = 1996338801)
+		assert t.responsavel is p1
+		
+		t = Telefone.get_by(numero = 1239134306)
+		assert not t.responsavel is p1
+
+
+	def test_moradores(self):
+		r = Republica.get_by(nome = 'Jerônimo')
+		m = r.moradores(date(2007, 03, 02))
+		
+		assert Pessoa.get_by(nome = 'André') in m
+		assert Pessoa.get_by(nome = 'Marcos') in m
+
+	def test_ultimo_fechamento_contas(self):
+		r = Republica(nome = 'Teste',
+			data_criacao = date.today(),
+			logradouro = 'R. dos Bobos, nº 0')
+			
+		objectstore.flush()
+		
+		assert date.today() == r.ultimo_fechamento()
+		
+		for delta in range(1, 4):
+			Fechamento(data = (date.today() - relativedelta(months = delta)), republica = r)
+		objectstore.flush()
+		
+		data_fechamento = date.today() - relativedelta(months = 1)
+		assert data_fechamento == r.ultimo_fechamento()
+		
+		
+
+
+
+	def test_importacao_conta_telefone_csv(self):
+		pass
