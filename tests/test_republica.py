776ef8f5a73156d4554725427f3b3e6a7866fa7b
andref <none@none>
2007-05-23 15:51:03 +0000

    Resolve #17 - Novos testes unitários para `Repúlbica`

--- tests/test_republica.py
+++ tests/test_republica.py
@@ -81,3 +81,90 @@ class TestRepublica(BaseTest):
 		r = Republica.get_by(id = 1)
 		
 		assert (date(2007, 5, 10), date(2007, 6, 6)) == r.ultimo_periodo_fechamento()
+	
+	
+	def test_contas_telefone(self):
+		r1 = Republica(nome = 'Teste1', data_criacao = date(2007, 4, 8), logradouro = 'R. dos Bobos, nº 0')
+		r2 = Republica(nome = 'Teste2', data_criacao = date(2007, 5, 8), logradouro = 'R. dos Bobos, nº 1')
+		
+		c1 = ContaTelefone(telefone = 11, companhia = 1, emissao = date(2007, 4, 29), vencimento = date(2007, 5, 10), republica = r1)
+		c2 = ContaTelefone(telefone = 11, companhia = 2, emissao = date(2007, 5, 29), vencimento = date(2007, 5, 10), republica = r1)
+		c3 = ContaTelefone(telefone = 11, companhia = 2, emissao = date(2007, 6, 21), vencimento = date(2007, 5, 10), republica = r1)
+		c4 = ContaTelefone(telefone = 22, companhia = 1, emissao = date(2007, 4, 29), vencimento = date(2007, 5, 10), republica = r2)
+		c5 = ContaTelefone(telefone = 22, companhia = 1, emissao = date(2007, 5, 10), vencimento = date(2007, 5, 10), republica = r2)
+		
+		objectstore.flush()
+		
+		assert len(r1.contas_telefone(date(2007, 1, 8), date(2007, 4, 28))) == 0
+		assert len(r1.contas_telefone(date(2007, 4, 29), date(2007, 5, 29))) == 2
+		assert len(r1.contas_telefone(date(2007, 4, 29), date(2007, 6, 21))) == 3
+		assert c1 in r1.contas_telefone(date(2007, 4, 29), date(2007, 5, 29))
+		assert c2 in r1.contas_telefone(date(2007, 4, 29), date(2007, 5, 29))
+		assert c3 in r1.contas_telefone(date(2007, 4, 29), date(2007, 6, 21))
+		
+		assert len(r2.contas_telefone(date(2007, 4, 29), date(2007, 6, 29))) == 2
+	
+	
+	def test_moradores(self):
+		'''
+		Testa quais moradores entram ou não em um determinado período de fechamento.
+		
+		Escrito de outras formas, o período de apuração pode ser definido como:
+		   * `[dia/mês, dia/(mês+1)[`
+		   * `----[xxxxxx[`--->`
+		
+		Caos de apuração:
+		
+		                        Período de Apuração
+		-------------------[xxxxxxxxxxxxxxxxxxxxxxxxxxxxx[------------------>    Morador     |  Incluído
+		                   |                             |
+		-------[xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-->    Morador_1   |  X
+		                   |                             |
+		------------------------[xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-->    Morador_2   |  X
+		                   |                             |
+		-------[xxxxxxxxxxxxxxxxxxxxxxxxxx]--------------------------------->    Morador_3   |  X
+		                   |                             |
+		--------------------------[xxxxxxxxx]------------------------------->    Morador_4   |  X
+		                   |                             |
+		--------------------------------------------------------[xxxxxxxxx-->    Morador_5   |      
+		                   |                             |
+		-------------------------------------------------[xxxxxxxx]--------->    Morador_6   |      
+		                   |                             |
+		-------[xxxxxxxx]--------------------------------------------------->    Morador_7   |      
+		                   |                             |
+		---------[xxxxxxxxx]------------------------------------------------>    Morador_8   |  X
+		'''
+		r = Republica(nome = 'Teste1', data_criacao = date(2007, 4, 8), logradouro = 'R. dos Bobos, nº 0')
+		
+		p1 = Pessoa(nome = '1')
+		p2 = Pessoa(nome = '2')
+		p3 = Pessoa(nome = '3')
+		p4 = Pessoa(nome = '4')
+		p5 = Pessoa(nome = '5')
+		p6 = Pessoa(nome = '6')
+		p7 = Pessoa(nome = '7')
+		p8 = Pessoa(nome = '8')
+		
+		# período de apuração = 2007-03-10 até 2007-04-09
+		m1 = Morador(pessoa = p1, republica = r, data_entrada = date(2007, 2, 1))
+		m2 = Morador(pessoa = p2, republica = r, data_entrada = date(2007, 3, 20))
+		m3 = Morador(pessoa = p3, republica = r, data_entrada = date(2007, 2, 1), data_saida = date(2007, 3, 20))
+		m4 = Morador(pessoa = p4, republica = r, data_entrada = date(2007, 3, 20), data_saida = date(2007, 4, 4))
+		m5 = Morador(pessoa = p5, republica = r, data_entrada = date(2007, 4, 20))
+		m6 = Morador(pessoa = p6, republica = r, data_entrada = date(2007, 4, 10), data_saida = date(2007, 5, 4))
+		m7 = Morador(pessoa = p7, republica = r, data_entrada = date(2007, 2, 1), data_saida = date(2007, 3, 1))
+		m8 = Morador(pessoa = p8, republica = r, data_entrada = date(2007, 2, 1), data_saida = date(2007, 3, 10))
+		
+		objectstore.flush()
+		
+		moradores = r.moradores(date(2007, 3, 10), date(2007, 4, 9))
+		
+		assert m1 in moradores
+		assert m2 in moradores
+		assert m3 in moradores
+		assert m4 in moradores
+		assert m5 not in moradores
+		assert m6 not in moradores
+		assert m7 not in moradores
+		assert m8 in moradores
+
