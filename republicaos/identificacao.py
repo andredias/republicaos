b5578aec38bdd5e23bbdfb761b64ddfa13d47db9
andref <none@none>
2007-05-26 16:51:23 +0000

    Identity usando Elixir ao invés de SQLAlchemy

--- republicaos/identificacao.py
+++ republicaos/identificacao.py
@@ -1,117 +1,58 @@
-from datetime import datetime
-from sqlalchemy import *
-from turbogears.database import metadata, session
-from sqlalchemy.ext.assignmapper import assign_mapper
-from turbogears import identity
+from turbogears.database    import metadata, session
+from elixir                 import Unicode, DateTime, String, Integer
+from elixir                 import Entity, has_field, using_options
+from elixir                 import has_many, belongs_to, has_and_belongs_to_many
+from sqlalchemy             import ForeignKey
+from datetime               import datetime
 
+#
+# identity model
+# 
 
-
-# The identity schema.
-visits_table = Table('visit', metadata,
-    Column('visit_key', String(40), primary_key=True),
-    Column('created', DateTime, nullable=False, default=datetime.now),
-    Column('expiry', DateTime)
-)
-
-visit_identity_table = Table('visit_identity', metadata,
-    Column('visit_key', String(40), primary_key=True),
-    Column('user_id', Integer, ForeignKey('tg_user.user_id'), index=True)
-)
-
-groups_table = Table('tg_group', metadata,
-    Column('group_id', Integer, primary_key=True),
-    Column('group_name', Unicode(16), unique=True),
-    Column('display_name', Unicode(255)),
-    Column('created', DateTime, default=datetime.now)
-)
-
-users_table = Table('tg_user', metadata,
-    Column('user_id', Integer, primary_key=True),
-    Column('user_name', Unicode(16), unique=True),
-    Column('email_address', Unicode(255), unique=True),
-    Column('display_name', Unicode(255)),
-    Column('password', Unicode(40)),
-    Column('created', DateTime, default=datetime.now)
-)
-
-permissions_table = Table('permission', metadata,
-    Column('permission_id', Integer, primary_key=True),
-    Column('permission_name', Unicode(16), unique=True),
-    Column('description', Unicode(255))
-)
-
-user_group_table = Table('user_group', metadata,
-    Column('user_id', Integer, ForeignKey('tg_user.user_id')),
-    Column('group_id', Integer, ForeignKey('tg_group.group_id'))
-)
-
-group_permission_table = Table('group_permission', metadata,
-    Column('group_id', Integer, ForeignKey('tg_group.group_id')),
-    Column('permission_id', Integer, ForeignKey('permission.permission_id'))
-)
-
-
-class Visit(object):
-    """
-    A visit to your site
-    """
+class Visit(Entity):
+    has_field('visit_key', String(40), primary_key=True)
+    has_field('created', DateTime, nullable=False, default=datetime.now)
+    has_field('expiry', DateTime)
+    using_options(tablename='visit')
+    
+    @classmethod
     def lookup_visit(cls, visit_key):
         return Visit.get(visit_key)
-    lookup_visit = classmethod(lookup_visit)
 
-class VisitIdentity(object):
-    """
-    A Visit that is link to a User object
-    """
-    pass
+class VisitIdentity(Entity):
+    has_field('visit_key', String(40), primary_key=True)
+    belongs_to('user', of_kind='User', colname='user_id', use_alter=True)
+    using_options(tablename='visit_identity')
 
-class Group(object):
-    """
-    An ultra-simple group definition.
-    """
-    pass
+class Group(Entity):
+    has_field('group_id', Integer, primary_key=True)
+    has_field('group_name', Unicode(16), unique=True)
+    has_field('display_name', Unicode(255))
+    has_field('created', DateTime, default=datetime.now)
+    has_and_belongs_to_many('users', of_kind='User', inverse='groups')
+    has_and_belongs_to_many('permissions', of_kind='Permission', inverse='groups')
+    using_options(tablename='tg_group')
 
-class User(object):
-    """
-    Reasonably basic User definition. Probably would want additional
-    attributes.
-    """
+class User(Entity):
+    has_field('user_id', Integer, primary_key=True)
+    has_field('user_name', Unicode(16), unique=True)
+    has_field('email_address', Unicode(255), unique=True)
+    has_field('display_name', Unicode(255))
+    has_field('password', Unicode(40))
+    has_field('created', DateTime, default=datetime.now)
+    has_and_belongs_to_many('groups', of_kind='Group', inverse='users')
+    using_options(tablename='tg_user')
+    
+    @property
     def permissions(self):
         perms = set()
         for g in self.groups:
             perms = perms | set(g.permissions)
         return perms
-    permissions = property(permissions)
 
-    def by_email_address(klass, email): 
-        """
-        A class method that can be used to search users
-        based on their email addresses since it is unique.
-        """
-        return klass.get_by(users_table.c.email_address==email) 
-
-    by_email_address = classmethod(by_email_address)
-
-    def by_user_name(klass, username):
-        """
-        A class method that permits to search users
-        based on their user_name attribute.
-        """
-        return klass.get_by(users_table.c.user_name==username)
-    by_user_name = classmethod(by_user_name)
-
-class Permission(object):
-    """
-    A relationship that determines what each Group can do
-    """
-    pass
-
-assign_mapper(session.context, Visit, visits_table)
-assign_mapper(session.context, VisitIdentity, visit_identity_table,
-          properties=dict(users=relation(User, backref='visit_identity')))
-assign_mapper(session.context, User, users_table)
-assign_mapper(session.context, Group, groups_table,
-          properties=dict(users=relation(User, secondary=user_group_table, backref='groups')))
-assign_mapper(session.context, Permission, permissions_table,
-          properties=dict(groups=relation(Group, secondary=group_permission_table, backref='permissions')))
-
+class Permission(Entity):
+    has_field('permission_id', Integer, primary_key=True)
+    has_field('permission_name', Unicode(16), unique=True)
+    has_field('description', Unicode(255))
+    has_and_belongs_to_many('groups', of_kind='Group', inverse='permissions')
+    using_options(tablename='permission')
