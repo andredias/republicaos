d22a5816d59bcca946d58f6b7a82a1dafd223cd8
André Felipe Dias <andref.dias@pronus.eng.br>
2009-08-01 18:05:49 -0300

    Todos os testes estão rodando com nosetests -x -d

--- republicaos/tests/functional/test_appserver.py
+++
@@ -1,9 +0,0 @@
-from republicaos.tests import *
-
-class TestAppServer(TestController):
-    def test_index(self):
-        response = self.app.get('/')
-        # Test response...
-        #tgt = '''<span style="color:lime">repoze.what and repoze.who for Pylons</span>'''
-        #assert tgt in response
-    
--- republicaos/tests/test_banco_dados.py
+++ republicaos/tests/test_banco_dados.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from test_model.base import BaseTest
-from republicaos.model.business import *
-from elixir import session
+from republicaos.model import *
 from decimal import Decimal
 
 '''
@@ -11,40 +9,46 @@ A finalidade desses testes é garantir que o tipo Money criado para lidar com De
 no Postgres.
 '''
 
+class BaseTest(object):
+    '''
+    Procurar onde está a classe BaseTest original. Estava em test_model.base
+    '''
+    pass
+
 class BaseBancoDeDados(BaseTest):
-	def popula_dados(self):
-		r = Republica(nome = 'Teste',
-			data_criacao = date.today(),
-			logradouro = 'R. dos Bobos, nº 0')
-		
-		c = ContaTelefone(
-				telefone = 1111,
-				operadora_id = 1,
-				emissao = date(2007, 4, 29),
-				vencimento = date(2007, 5, 10),
-				republica = r,
-				franquia  = Decimal('12.23'),
-				servicos = Decimal(10)
-				)
-		session.flush()
-		session.clear()
-		
-	
-	def test_recuperacao(self):
-		self.popula_dados()
-		
-		r = Republica.get_by(id = 1)
-		c = ContaTelefone.get_by(id = 1)
-		
-		assert c.franquia == Decimal('12.23')
-		assert c.servicos == Decimal(10)
+    def popula_dados(self):
+        r = Republica(nome = 'Teste',
+            data_criacao = date.today(),
+            logradouro = 'R. dos Bobos, nº 0')
+        
+        c = ContaTelefone(
+                telefone = 1111,
+                operadora_id = 1,
+                emissao = date(2007, 4, 29),
+                vencimento = date(2007, 5, 10),
+                republica = r,
+                franquia  = Decimal('12.23'),
+                servicos = Decimal(10)
+                )
+        session.flush()
+        session.clear()
+        
+    
+    def test_recuperacao(self):
+        self.popula_dados()
+        
+        r = Republica.get_by(id = 1)
+        c = ContaTelefone.get_by(id = 1)
+        
+        assert c.franquia == Decimal('12.23')
+        assert c.servicos == Decimal(10)
 
 
 
-class TestSQLite(BaseBancoDeDados):
-	url = 'sqlite:///:memory:'
+class _TestSQLite(BaseBancoDeDados):
+    url = 'sqlite:///:memory:'
 
 
 
-class TestPostgres(BaseBancoDeDados):
-	url = 'postgres://turbo_gears:tgears@localhost/tg_teste'
+class _TestPostgres(BaseBancoDeDados):
+    url = 'postgres://turbo_gears:tgears@localhost/tg_teste'
