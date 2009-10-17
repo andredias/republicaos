1b9617ef21b9bf522e044561c9249a9e76017714
André Felipe Dias <andref.dias@pronus.eng.br>
2009-10-16 22:05:57 -0300

    Não é para todos os campos texto de formulários usarem placeholders. Só aqueles que forem explicitamente indicados.

--- republicaos/public/js/utils.js
+++ republicaos/public/js/utils.js
@@ -1,16 +1,16 @@
 function setInputPlaceholders() {
-    $('input[type=text]').focus(
+    $('input[type=text][class="placeholder"]').focus(
         function(){
             if($(this).val() == $(this).attr('defaultValue'))
-                $(this).val('').removeClass('placeholder')
+                $(this).val('').removeClass('_placeholder')
         }
     );
       
-    $('input[type=text]').blur(
+    $('input[type=text][class="placeholder"]').blur(
         function()
             {
             if($(this).val() == '')
-                $(this).val($(this).attr('defaultValue')).addClass('placeholder');
+                $(this).val($(this).attr('defaultValue')).addClass('_placeholder');
             }
     );
 }
